76f41ab93adde90a6f84c4181c3ca2ee29b3cd12
raymondintervalo <raymondmatthewintervalo@gmail.com>
2024-03-26 02:06:25 +0800

    implemented technician availability logic and implemented in set ocular page

---
+++ client/src/hooks/useAvailableTechnicians.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useAvailableTechnicians = (datetime) => {
+    const [allTechnicians, setAllTechnicians] = useState([])
+    const [availTechnicians, setAvailTechnicians] = useState([])
+    const [checked, setChecked] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/api/getTechnicians/')
+                setAllTechnicians(response.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        if (datetime !== null && allTechnicians.length > 0) {
+            const fetchAvailability = async () => {
+                try {
+                    const promises = allTechnicians.map(async (technician) => {
+                        const valid = await axios.get(`http://localhost:4000/api/getTechnicianAvailability/${technician.technician_id}`,{ params: {datetime: datetime} } );
+                        return valid.data;
+                    });
+    
+                    const results = await Promise.all(promises);
+                    const availTechniciansData = allTechnicians.filter((technician, index) => results[index]);
+                    
+    
+                    setAvailTechnicians(availTechniciansData);
+                    setChecked(true)
+                } catch (error) {
+                    console.error(error);
+                }
+            };
+    
+            fetchAvailability();
+        }
+    }, [datetime, allTechnicians]); // Run when datetime or allTechnicians change
+
+    const technicians = checked ? availTechnicians : allTechnicians
+    const mod = checked && availTechnicians.length !== allTechnicians.length
+    console.log(technicians, mod)
+    return {technicians, mod}
+
+}
+
+export default useAvailableTechnicians
